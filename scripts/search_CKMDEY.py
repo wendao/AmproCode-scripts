d9ec122fc55d6a5d9f98f396e68c73087272ac13
Yuan Liu <pandawendao@gmail.com>
2023-08-23 12:05:57 +0800

    Adding new features
    1. group by same protein id
    2. rank with redundancy

--- scripts/search_CKMDEY.py
+++ scripts/search_CKMDEY.py
@@ -32,7 +32,7 @@ code = []
 
 for gene in recorddic.keys():
   record = recorddic[gene]
-  prot.append(record.id)
+  prot.append("|".join(record.id.split('|')[:-1]))
   seq = record.seq
   nC = seq.count('C')
   nM = seq.count('M')
@@ -40,12 +40,12 @@ for gene in recorddic.keys():
   nD = seq.count('D')
   nE = seq.count('E')
   nY = seq.count('Y')
-  n = gcd( nC, nM, nK+1, nD+nE+1 )
+  n = gcd( nC, nM, nK, nD+nE+1, nY)
   code.append( [ float(nC)/n,
                  float(nM)/n,
                  float(nK)/n,
                  float(nD+nE+1)/n,
-                 float(nY)/n])
+                 float(nY)/n ])
 
 N = len(prot)
 code = np.asarray(code)
@@ -60,7 +60,38 @@ dist_ndx = [(d,i) for i,d in enumerate(dist)]
 dist_ndx = sorted(dist_ndx, key=lambda x : x[0])
 
 r = 0
-for d, i in dist_ndx[:10]:
-    r += 1
-    print( "rank=", r, prot[i], d)
+check_id = []
+save_top = []
+for d, i in dist_ndx:
+    if prot[i] in check_id:
+        continue
+    else:
+        r += 1
+        check_id.append(prot[i])
+        save_top.append( (r, prot[i], d) )
+        if r==10: break
+save_top.append( (11, "FAKE", 999) ) #close
 
+map_redundancy = {}
+last_dist = -1.0
+last_rank = 0
+redundancy = 1
+for r, p, d in save_top:
+    #print("#", r, p, d )
+    if d-last_dist < 1e-6:
+        #same level
+        #print("same level")
+        redundancy += 1
+    else:
+        #new level
+        #print("new level")
+        if last_rank>0:
+            #print("check:", last_rank, r)
+            for i in range(last_rank, r):
+                map_redundancy[i] = redundancy
+        last_rank = r
+        last_dist = d
+        redundancy = 1
+
+for rank, redundancy in map_redundancy.items():
+    print("rank=", rank, save_top[rank-1][1], save_top[rank-1][2], redundancy)
